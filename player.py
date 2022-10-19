e015899bab949e46001d68ad74d9dd8ac4429c0a
CelisseRoberts <celisse@ymail.com>
2022-10-19 16:13:00 -0400

    Modified player.py file

--- player.py
+++ player.py
@@ -7,3 +7,11 @@ class Player:
         self.player_one = self.human
     input('Please select number of players: 1, 2, or 3: ') 
    
+    
+    def player_two(self): 
+        self.player_two = self.ai
+    print('Player 2 is an ai player')
+
+    def player_three(self):
+        self.player_three = ['Human, AI']
+    input('Choose 1 for human player or 2 for ai player: ' )
